0d91d3e82e463730b0b7fe4b9b51397f2f0abaaa
nislep0 <ruden1603@gmail.com>
2025-04-17 20:27:57 +0300

    realise numpy +other variants

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy as np
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,14 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrices')
+def multiply_matrices():
+    a = np.random.randint(0, 10, size=(10, 10)).tolist()
+    b = np.random.randint(0, 10, size=(10, 10)).tolist()
+    product = (np.matmul(a, b)).tolist()
+    return {
+        "matrix_a": a,
+        "matrix_b": b,
+        "product": product
+    }
